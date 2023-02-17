68dd8067265b0d66c4cc841dc3ef7e45a2cf79bb
saif-sabit <saifsabit@gmail.com>
2023-02-16 18:33:46 -0800

    Task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print String and exit status 1' 
+ * Return: Always 1 (fail)
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
