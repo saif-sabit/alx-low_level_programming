06dd8c4f3e082bc7e97f5b3ae60267f592049f16
saif-sabit <saifsabit@gmail.com>
2023-02-17 12:57:27 -0800

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Program that prints positive or negative or zero using printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		for (m = n + 1; m < 9;m++ )
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+			if (!(n == m))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
