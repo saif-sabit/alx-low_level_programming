bfb5cb689eba6b476f0d92eefb241f5baaeabc54
saif-sabit <saifsabit@gmail.com>
2023-02-16 18:38:45 -0800

    Task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'print String and exit status 1' 
+ * Description: 'Program that exit status code is 1 with error message'
  * Return: Always 1 (fail)
  */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprint("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
