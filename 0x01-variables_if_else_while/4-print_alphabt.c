d93bce18a5d18313da616d2695898c8c472e1b52
saif-sabit <saifsabit@gmail.com>
2023-02-17 09:13:14 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Program that prints positive or negative or zero using printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+	if( c !='e' or c !='q')
+	putchar(c);
+}
+	putchar('\n');
+	return (0);
+
+}
