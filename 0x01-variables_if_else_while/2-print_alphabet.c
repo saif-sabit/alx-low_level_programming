cba92079a80f9cc3c7a712070bce7614ddb187f2
saif-sabit <saifsabit@gmail.com>
2023-02-17 08:40:05 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Program that prints positive or negative or zero using printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char c='a';c <= 'z';c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+
+}
