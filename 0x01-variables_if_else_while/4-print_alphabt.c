4a0182d71c4fbaa5870cbd04ec864603d36dbd57
saif-sabit <saifsabit@gmail.com>
2023-02-17 09:51:57 -0800

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,13 @@ int main(void)
 char c;
 for (c = 'a'; c <= 'z'; c++)
 {
-	if (!((c = 'e') || (c = 'q')))
+	if ((c = 'e') || (c = 'q'))
+	{
+	}
+	else
+	{
 		putchar(c);
+	}
 }
 	putchar('\n');
 	return (0);
