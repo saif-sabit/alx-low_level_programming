0e31c3cdb1816f5b7297e48b41c6d278adee4bed
saif-sabit <saifsabit@gmail.com>
2023-02-21 12:04:54 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "saif.h"
+/*
+ *
+ *
+ *always return 0
+ */
+void main() {
+	int i=o;
+	char* str="_putchar";
+	while(str[i] !='\0') {
+
+		_putchar('a');
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This project is about 
+Learning about functions 
+Nested loops and how to use them.
