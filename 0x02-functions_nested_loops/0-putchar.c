424628f91316bc5c972d4d17f0c8f813e6c8276f
saif-sabit <saifsabit@gmail.com>
2023-02-21 14:32:46 -0800

    Task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,13 @@
  *
  *always return 0
 */
-int main(void){
-	int i=0;
+int main(void)
+{
+	int i = 0;
 	char* str = "_putchar";
-	while(str[i] != '\0'){
+
+	while (str[i] != '\0') 
+	{
 		_putchar(str[i]);
 		i++;
 	}
