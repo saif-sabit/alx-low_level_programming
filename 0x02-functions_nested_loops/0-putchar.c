201f93ac0cce8705f298e104e4ddf0998170a5c2
saif-sabit <saifsabit@gmail.com>
2023-02-21 13:12:53 -0800

    T

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,13 +4,7 @@
  *
  *always return 0
  */
-int _putchar(char c);
 int main() {
-	int i = 0;
-	char* str = "_putchar";
-	while(str[i] !='\0') {
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+	_putchar('a');
 	return (0);
 }
