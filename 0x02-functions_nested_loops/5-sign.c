69c0bfb9fb0933845702ef71383b36771da0bdea
saif-sabit <saifsabit@gmail.com>
2023-02-21 17:37:52 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * print_sign - Return 1 if number is positive and 0 if zero and -1 if negative
- * @c: char type number
- * always Returns  1,-1 or 0
- */
+ * @c: char type numbe
+ *Return: 1 if positive, 0 if 0 zero, and -1 if negative
+ * */
 int print_sign(int c)
 {
 	if (c > 0)
