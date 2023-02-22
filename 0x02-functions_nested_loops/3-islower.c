445acce075137b161043382feab667560f590a19
saif-sabit <saifsabit@gmail.com>
2023-02-21 16:16:32 -0800

    Task 2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *int _islower(int c)
- *Description :prints letters from a to z times
+ * _islower checks ifd letter is uppercase
+ * accepts @paraqmeter c is a character
  * Return: Always void
  */
 
