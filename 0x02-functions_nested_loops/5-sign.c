8286621aec44b60c2b3b891617da5fadd28021a9
saif-sabit <saifsabit@gmail.com>
2023-02-21 17:23:02 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - Return 1 if number is positive and 0 if zero and -1 if negative
+ * @c: char type number
+ */
+int print_sign(int c)
+{
+	if (c > 0){
+		_putchar('+');
+		return (1);
+	}
+	else if (c < 0)
+	{
+		_putchar('-');
+		return(-1);
+	}
+	else
+	{
+	_putchar('0');
+	return (0);
+	}
+}
