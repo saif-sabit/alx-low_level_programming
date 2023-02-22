b1458722f441f9a3343d4c10147db9664adaabf4
saif-sabit <saifsabit@gmail.com>
2023-02-21 17:25:55 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,16 +5,16 @@
  */
 int print_sign(int c)
 {
-	if (c > 0){
-		_putchar('+');
-		return (1);
+	if (c > 0)
+	{
+		_putchar(1);
+		return ('+');
 	}
 	else if (c < 0)
 	{
 		_putchar('-');
-		return(-1);
-	}
-	else
+		return (-1);
+	}else
 	{
 	_putchar('0');
 	return (0);
