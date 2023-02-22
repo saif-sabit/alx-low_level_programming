729c6e59c10971ca144df767c5f5c32a1f541943
saif-sabit <saifsabit@gmail.com>
2023-02-21 18:05:55 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,15 @@
-#
+#include "main.h"
+/**
+ * print_last_digit - Return last digit
+ * @c: int type
+ *Return: last digit
+ */
+int print_last_digit(int c)
+{
+	while(c > 9)
+	{
+		c = c % 10;
+	}
+	_putchar('c');
+	return (c);
+}
