fa8205623182dbb883a5f125448365cc0feb8c21
saif-sabit <saifsabit@gmail.com>
2023-02-22 01:40:21 -0800

    Advanced

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,12 +3,13 @@
  * main - entry point
  * program lists and sums  all the natural numbers below 10
  * that are multiples of 5 or 3
- * always return 0
+ * Return: always return 0
  */
-int main()
+int main(void)
 {
 	int i;
 	int sum = 0;
+
 	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
