01fbeebfc44abb6d2e47ea41288586f73852e382
saif-sabit <saifsabit@gmail.com>
2023-02-24 14:05:58 -0800

    Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
-#ifndef INV_TREE_H
-#define INV_TREE_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 void positive_or_negative(int i);
-int convert_day(int month, int day);
-int largest_number(int a, int b, int c);
-#endif
+
+#endif /* MAIN_H */
