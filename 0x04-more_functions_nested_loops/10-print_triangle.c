32080c57960938eee81acd82f5f151cfbac77a8c
saif-sabit <saifsabit@gmail.com>
2023-02-24 13:27:28 -0800

    Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,17 +7,22 @@ void print_triangle(int size)
 {
     int i;
     int j;
+    int v;
     i = 0;
     j = 0;
+    v = 0;
 
     if (size > 0)
     {
-        for (i = 0; i <= size; i++)
+        for (i = 0; i < size; i++)
         {
-            for(j = 0; j < i; j++)
-                _putchar('#');
-            _putchar('\n');            
+            for (v = size - 1 - i; v > 0 ; v--)
+            _putchar(' ');
+            for (j = 0; j < i+1; j++)
+            _putchar('#');
+            _putchar('\n');
         }
+        
     }
     else
     {
