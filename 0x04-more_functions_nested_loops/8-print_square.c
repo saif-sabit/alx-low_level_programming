228dfc7fe4a511b7d04c20421e3209abbeeeb165
saif-sabit <saifsabit@gmail.com>
2023-02-24 10:05:30 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square prints square 
+ * @c int type number
+ * print square
+*/
+void print_square(int size)
+{
+    int i;
+    int j;
+    i = 0;
+    j = 0;
+
+    if(size >= 0)
+    {
+        for(i = 0; i < size; i++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+    else
+    {
+        _putchar('\n');
+    }
+}
