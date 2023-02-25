61b0db791004a01856d2abc595a0ba781992fe3b
saif-sabit <saifsabit@gmail.com>
2023-02-24 16:53:49 -0800

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
-*_isupper 
-*@c: int type number
-*always return 0 if not upper else return 1
-*/
+ * _isupper - checks if char is upper
+ * @c: int type number
+ * Return: 0 if not upper else return 1
+ */
+
 int _isupper(int c)
 {
-    if(c > 64 && c < 91)
-    return(1);
-    else
-    return (0);
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
 }
