20eb7bb4b009035e8d671fccb4edffe50fd796fd
saif-sabit <saifsabit@gmail.com>
2023-02-24 18:42:06 -0800

    Betty

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,11 +8,19 @@
 void print_diagonal(int n)
 {
 	int i;
+	int v;
 
 	i = 0;
+	v = 0;
 
 	if (n >= 0)
+	{
 		for (i = 0; i < n; i++)
+		{
+			for (v = n - 1 - i; v > 0; v--)
+				_putchar(' ');
 			_putchar('\\');
-	_putchar('\n');
+			_putchar('\n');
+		}
+	}
 }
