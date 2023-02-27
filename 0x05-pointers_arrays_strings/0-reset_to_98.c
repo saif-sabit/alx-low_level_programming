d77e80c9e6133e5a9ba86ab51236d3509ca98dfe
saif-sabit <saifsabit@gmail.com>
2023-02-27 02:56:50 -0800

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98: reset value to 98
- * @*n: of type pointer 
- * return nothing
+ * reset_to_98 - reset value to 98
+ * @n: of type pointer
+ * Description:'set value to 98'
+ * return: nothing
  */
 void reset_to_98(int *n)
 {
