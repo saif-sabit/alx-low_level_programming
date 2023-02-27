cacb4f58c2b8f4673f8bdc90a7bcf17789fbe8dc
saif-sabit <saifsabit@gmail.com>
2023-02-27 08:52:43 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - print reverse string
+ * @s: char type string
+ * Returns nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s); i > = 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
