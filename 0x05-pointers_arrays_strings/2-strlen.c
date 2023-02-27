3cf07e4eaa4badd1feadd5db5e037d54044c1712
saif-sabit <saifsabit@gmail.com>
2023-02-27 07:46:35 -0800

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,9 +10,10 @@ int _strlen(char *s)
 	int count;
 
 	count = 0;
-	while (*s != '\0')
+	while (s[count] != '\0')
 	{
 		count++;
+
 	}
 	return (count);
 
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts
+ * @str : char type string
+ *return: nothing
+ */
+void _puts(char *str)
+{
+
+}
