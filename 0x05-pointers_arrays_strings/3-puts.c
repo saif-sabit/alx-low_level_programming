ad72fdc5cfd08b78f0080ff3c2776f314d0a3341
saif-sabit <saifsabit@gmail.com>
2023-02-27 07:50:21 -0800

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,19 @@
 #include "main.h"
 /**
- * _puts
+ * _puts- display string
  * @str : char type string
  *return: nothing
  */
+
 void _puts(char *str)
 {
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
 
 }
