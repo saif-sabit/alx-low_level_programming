94d282a3ff46082cdcda7201b4a570999ae1e02a
saif-sabit <saifsabit@gmail.com>
2023-02-27 10:09:19 -0800

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2- display string
+ * @str : char type string
+ *return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+
+}
