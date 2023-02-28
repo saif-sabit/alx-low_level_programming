5ad5621ffa330ee0ebb229e800c3218fcf8f0a94
saif-sabit <saifsabit@gmail.com>
2023-02-28 00:21:23 -0800

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -19,7 +19,7 @@ void rev_string(char *s)
 	for (i = 0; i < (count - 1) / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[count - i - 1];
-		s[count -  i - 1] = temp;
+		s[i] = s[count - i - 2];
+		s[count -  i - 2] = temp;
 	}
 }
