00c900c0ce536cb6966f8dcae27860e2dc65cb93
saif-sabit <saifsabit@gmail.com>
2023-03-01 11:17:26 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,7 @@
  * Return - pointer of copied string
  */
 
-char *_strncpy(char *dest, const char *src, unsigned int n)
+char *_strncpy(char *dest, const char *src, int n)
 {
 	int i;
 	for (i = 0; i < n && src[i] != '\0'; i++)
