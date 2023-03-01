fddf9057441f4b70e360aff34266b9a1de641b66
saif-sabit <saifsabit@gmail.com>
2023-03-01 11:31:00 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,12 +6,16 @@
  * @n: unsigned int type number
  * Return - pointer of copied string
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
+	i = 0;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
-	return dest;
+	return (dest);
 }
