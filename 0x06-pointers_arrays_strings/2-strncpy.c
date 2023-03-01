cfd6ede5dddf812fd0adb0075b9fd0423723d927
saif-sabit <saifsabit@gmail.com>
2023-03-01 11:13:45 -0800

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - copies particular amount of string into another string pointer
+ * @dest: char pointer type string
+ * @src: char pointer type string
+ * @n: unsigned int type number
+ * Return - pointer of copied string
+ */
+
+char *_strncpy(char *dest, const char *src, unsigned int n)
+{
+	int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return dest;
+}
