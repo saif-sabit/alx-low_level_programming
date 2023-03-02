9b678b5cd6981e88daab358985f363d83c9ed0b1
saif-sabit <saifsabit@gmail.com>
2023-03-01 23:14:32 -0800

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,20 @@
 #include "main.h"
 /**
  * reverse_array - takes an array of integer and reverse the order
- * @a: pointer to array of type int 
+ * @a: pointer to array of type int
  * @n: int number of elements
  */
 void reverse_array(int *a, int n)
 {
 	int i;
+	int temp;
+
 	i = 0;
 
-	while (i < (n/2))
+	while (i < (n / 2))
 	{
-		int temp = a[i];
+		temp = a[i];
+
 		a[i] = a[n - i - 1];
 		a[n - i - 1] = temp;
 		i++;
