a7e1a679940c5a8fad29e4b4d5e39e0228e1ecca
saif-sabit <saifsabit@gmail.com>
2023-03-02 14:08:36 -0800

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - all lower case into upper
+ * @s :pointer type char
+ * return : pointer of type char
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] > 96 && s[i] < 123)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
