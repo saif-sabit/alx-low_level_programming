306635526f522432d53a665ccb601fa5ec61c8ee
saif-sabit <saifsabit@gmail.com>
2023-03-02 15:44:26 -0800

    ta

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,11 +20,13 @@ char *cap_string(char *s)
 			if (k == -1 && s[i] > 96 & s[i] < 123)
 				s[i] = s[i] - 32;
 			else if (s[i] == a[j] || s[i] == 9 || s[i] == 10)
+			{
 				if (s[i + 1] > 96 && s[i + 1] < 123)
 				{
 					s[i + 1] = s[i + 1] - 32;
 					break;
 				}
+			}
 		}
 		k = 0;
 		i++;
