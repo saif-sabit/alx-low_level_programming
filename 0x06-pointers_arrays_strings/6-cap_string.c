a199094ca4b5e8c3ab6712e10cb3ebeaa3cc6e49
saif-sabit <saifsabit@gmail.com>
2023-03-02 14:58:52 -0800

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - Capitaize
+ * @s: pointer to char string
+ * Return: pointer to string after capitalize
+ */
+char *cap_string(char *s)
+{
+	char a[] = { ',', ';', ' ', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j] || s[i] == 9 || s[i] == 10)
+				if (s[i + 1] > 96 && s[i + 1] < 123)
+				{
+					s[i + 1] = s[i + 1] - 32;
+					break;
+				}
+		}
+		i++;
+	}
+	return (s);
+}
