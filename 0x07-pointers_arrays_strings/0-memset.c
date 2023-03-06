6d7f13b2187e8ac1dc7b0b988e108ec61676aed0
saif-sabit <saifsabit@gmail.com>
2023-03-06 06:52:49 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer type char
+ * @b: pointer type char
+ * @n: unsigned int number
+ * Return: pointer to memory address
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	j = 0;
+
+	for (j = 0; j < n; j++)
+	{
+		s[j] = b;
+	}
+
+	retrun(s);
+}
