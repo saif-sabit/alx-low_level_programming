5a72e842c0c0c044717fa82baa197d92b8d94cf0
saif-sabit <saifsabit@gmail.com>
2023-03-07 15:31:26 -0800

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _strpbrk - search source string about occurance in s
+ * @s: pointer to source string
+ * @accept: pointer to value to be scanned from
+ * Retrun: pointer to string from first occurance of s letters
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *n;
+	unsigned int i;
+	unsigned int j;
+	int f;
+
+	n = s;
+
+	i = f = j = 0;
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				f = 1;
+				break;
+			}
+			j++;
+		}
+		if (f == 1)
+			break;
+		j = 0;
+		n++;
+		i++;
+	}
+	if (f == 0)
+		return (NULL);
+	else
+		return (n);
+
+}
