fbdff8be810c1fa3646b5ddf9eb83d5276489059
saif-sabit <saifsabit@gmail.com>
2023-03-08 13:57:34 -0800

    Advaned

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * is_palindrome - shows if string is palindrome
+ * @s: string pointer
+ * Return: 0 when it is plaindrome
+ */
+int is_palindrome(char *s)
+{
+return (0);
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * wildcmp - checks if string one and two are the same
+ * @s1: char pointer
+ * @s2: char pointer string 2
+ * Return: 1 if words are identical else 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
