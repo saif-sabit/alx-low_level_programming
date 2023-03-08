058e648cddf96af9e99debcf6c4029891251ae00
saif-sabit <saifsabit@gmail.com>
2023-03-08 13:58:41 -0800

    Man

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - prints sqrt
+ * @n: int num
+ * Return: sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	return (0);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * is_prime)number
+ * @n: int number
+ * Return: 1 if n is prime
+ */
+int is_prime_number(int n)
+{
+
+	return (0);
+}
