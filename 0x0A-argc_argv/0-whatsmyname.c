423a922f743eab0fa49f1b4e0da85a00a876bf14
saif-sabit <saifsabit@gmail.com>
2023-03-10 15:24:03 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints your name
+ * main- prints your name
  * @argc: number of command line args
  * @argv: the actual args
- * Returns: always 0
+ * Return: always 0
  */
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[argc - 1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
