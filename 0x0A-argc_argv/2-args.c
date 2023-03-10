83cab04717cee6ad6dae59338231d5a6841468bf
saif-sabit <saifsabit@gmail.com>
2023-03-10 15:59:24 -0800

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main- prints all args
+ * @argc: number of command line args
+ * @argv: the actual args
+ * Return: always return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	i = 0;
+	for(i = 0; i<argc, i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
