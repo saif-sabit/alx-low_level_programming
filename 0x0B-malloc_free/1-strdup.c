0b2285413399ea8e7d5e216098758a7f58ed48f9
saif-sabit <saifsabit@gmail.com>
2023-03-13 12:32:08 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * _strdup - duplicates string
+ * @str: charf pointer for string
+ * Return: pointer to duplicated string or null if fails
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, size;
+
+	i = 0;
+	*p = (char *) '0';
+
+	if (str[i] == '\0')
+		return (NULL);
+		while (str[i] != '\0')
+			i++;
+		size = i;
+		p = (char *)malloc(size * sizeof(char));
+		if (p)
+		{
+			for (i = 0; i < = size; i++)
+				p[i] = str[i];
+			return (p);
+		}
+		else
+		{
+			return (NULL);
+		}
+}
