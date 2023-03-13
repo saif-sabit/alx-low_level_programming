0e360c8176d1bba5491d5ae8e5ce52fb76d97def
saif-sabit <saifsabit@gmail.com>
2023-03-13 01:24:17 -0700

    Task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,24 +3,26 @@
  * *create_array- creates array
  * @size: number considered size of array
  * @c: charto fill array with
- * Return: always return pointer to array or null 
+ * Return: always return pointer to array or null
  */
 
- char *create_array(unsigned int size, char c)
- {
-	 int i;
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *p;
 
-	 i = 0;
+	p = (char *)0;
+	i = 0;
 
-	 if ( size <= 0)
-	 {
-		 return (NULL);
-	 }
-	 else
-	 {
-		 char *p = (char*) malloc(size * sizeof(char));
-		 for (int i = 0; i <size; i++)
-			 p[i] = c;
-		 return (p);
-	 }
- }
+	if (size < 1)
+	{
+		return (p);
+	}
+	else
+	{
+		p = (char *) malloc(size * sizeof(char));
+		for (i = 0; i < size; i++)
+			p[i] = c;
+		return (p);
+	}
+}
