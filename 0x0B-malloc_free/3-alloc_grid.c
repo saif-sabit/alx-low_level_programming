07c0837f15825c28ea77e695b6c6b37e980934d9
saif-sabit <saifsabit@gmail.com>
2023-03-14 16:15:53 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,10 +23,7 @@ int **alloc_grid(int width, int height)
 		p[i] = malloc(sizeof(int) * width);
 		if (p[i] == NULL)
 		{
-			for (i - 1; i >= 0; i--)
-			{
-				free(p[i]);
-			}
+			free(p[i]);
 			free(p);
 			return (NULL);
 		}
