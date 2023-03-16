ec4174d6a88b06bffc60a1e7432fe63506f2d54d
saif-sabit <saifsabit@gmail.com>
2023-03-16 13:00:45 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *p;
-	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -19,10 +18,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (!p)
 	{
 		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-	{
-		p[i] = 0;
 	}
 	return (p);
 }
