7aa8ab46dd048adb1cb67a0734a5608d605c6d1e
saif-sabit <saifsabit@gmail.com>
2023-03-16 12:01:37 -0700

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for nmemb element
+ * @nmemb: number of elements
+ * @size: size of block
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (!p)
+	{
+		return (NULL);
+	}
+	return (p);
+}
