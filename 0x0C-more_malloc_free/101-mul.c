c32a17191e5cee7e57332a9ee5390d6798ce3d1c
saif-sabit <saifsabit@gmail.com>
2023-03-17 11:04:47 -0700

    Task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,35 +2,31 @@
 #include <stdlib.h>
 int is_number(char *p)
 {
-	int i;
+	unsigned int i;
 	
 	i = 0;
-    while(p[i] != '\0')
-    {
-        if (!(p[i] > 47 && p[i] < 58))
+	while(p[i] != '\0')
 	{
-            return (1);
+		if (!(p[i] > 47 && p[i] < 58))
+			return (1);
+		i++;
 	}
-            i++;
-    }
-    return (0);
+	return (0);
 }
+/**
+ *
+ *
+ */
+
 int main(int argc, char *argv[])
-{        
-	if (is_number(argv[1])== 1 || is_number(argv[2])==1)
-	{
-        	printf("Error\n");
-		exit(98);
-        }
+{
 	if (argc == 3)
 	{
-        int i = atoi(argv[1]);
-        int j = atoi(argv[2]);
-        if (i == 0 || j == 0)
-        {
-        printf("Error\n");
-		exit(98);
-        }
+		if (is_number(argv[1])== 1 || is_number(argv[2])==1)
+		{
+			printf("Error\n");
+			exit(98);
+		}
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
