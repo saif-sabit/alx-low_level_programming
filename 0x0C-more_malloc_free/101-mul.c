18741a1f77315b3727e5737f403b5f6430697038
saif-sabit <saifsabit@gmail.com>
2023-03-17 11:14:33 -0700

    Task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * is_number - detects if digit is number
+ * @p: pointer to string
+ * Return: 1 if it is all number or 0 if there is a char
+ */
+
 int is_number(char *p)
 {
 	unsigned int i;
-	
+
 	i = 0;
-	while(p[i] != '\0')
+	while (p[i] != '\0')
 	{
 		if (!(p[i] > 47 && p[i] < 58))
 			return (1);
@@ -14,15 +20,17 @@ int is_number(char *p)
 	return (0);
 }
 /**
- *
- *
+ * main- entry point
+ * @argc: no of args
+ * @argv: array for args
+ * Return: 0 in successful execution
  */
 
 int main(int argc, char *argv[])
 {
 	if (argc == 3)
 	{
-		if (is_number(argv[1])== 1 || is_number(argv[2])==1)
+		if (is_number(argv[1]) == 1 || is_number(argv[2]) == 1)
 		{
 			printf("Error\n");
 			exit(98);
