05bac34b9ad611a7d081b1f090be5e96489567f2
saif-sabit <saifsabit@gmail.com>
2023-03-20 09:04:02 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 
 	d = malloc(sizeof(dog_t));
+	if (!d)
+		return (NULL);
 	if (name == NULL || owner == NULL || age < 0)
 		return (NULL);
 	d->name = name;
