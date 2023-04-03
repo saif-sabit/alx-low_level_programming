e6b59c288a0f3502f0589576829ab4485fe48376
saif-sabit <saifsabit@gmail.com>
2023-04-03 16:21:28 -0700

    Free list int 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+void free_listint2(listint_t **head)
+{
+	listint_t *temp ;
+	listint_t *tmp;
+
+	temp = (*head);
+	while(temp)
+	{
+		tmp = temp;
+		temp = temp->next;
+		free(tmp);
+	}
+	(*head) = NULL;
+
+}
