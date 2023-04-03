906ffba23e10a37ab6264c54195a02bfab89a499
saif-sabit <saifsabit@gmail.com>
2023-04-03 15:43:07 -0700

    Free linkedlist

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,10 +7,11 @@
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
-        while (head)
-        {
+
+	while (head)
+	{
 		temp = head;
 		head = head->next;
-                free(temp);
-        }
+		free(temp);
+	}
 }
