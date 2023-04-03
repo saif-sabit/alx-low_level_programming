71fafe97e644699bf68bc1be7090f6a006d5867b
saif-sabit <saifsabit@gmail.com>
2023-04-03 00:53:45 -0700

    List of integer length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * listint_len - prints list contents
+ * @h: pointer to a list
+ * Return: size of printed list
+ */
+size_t listint_len(const listint_t *h)
+{
+            size_t i = 0;
+            while (h != NULL)
+            {
+            h = h->next;
+            i++;
+            }
+            return (i);
+}
