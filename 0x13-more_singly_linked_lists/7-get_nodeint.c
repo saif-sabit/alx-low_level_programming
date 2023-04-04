11f6a0016f8eb0aa6e8f4fddb085493903388d0d
saif-sabit <saifsabit@gmail.com>
2023-04-04 15:29:58 -0700

    Get index at given Index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_nodeint_ata_index- returns node in given index
+ * @head: pointer to int list
+ * @index: meant index to be searched
+ * Return: node at given index or NULL if doesn't exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *retlis;
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (index == i)
+		{
+			retlis = head;
+			return (retlis);
+		}
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
