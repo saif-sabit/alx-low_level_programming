7c275420e7543266c467bb7a49d52f1b5a1fc554
saif-sabit <saifsabit@gmail.com>
2023-04-07 16:59:53 -0700

    HEADER file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+#include <stddef.h>
+#include <stdlib.h>
+#include <sys/types.h>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
