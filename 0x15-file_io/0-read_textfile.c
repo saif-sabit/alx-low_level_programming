0fff88cee0cbbed101770c790ec45a068ce02276
saif-sabit <saifsabit@gmail.com>
2023-04-07 17:00:21 -0700

    Read from text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * read_textfile- Reads text file
+ * @filename: name of the file to be read
+ * @letters: number of letters to be read and written
+ * Return: Number of letters read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int n, fd;
+	buf = malloc(letters * sizeof(char));
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	n = read(fd, buf, letters);
+	write(1, buf, n);
+	close(fd);
+	return (n);
+}
