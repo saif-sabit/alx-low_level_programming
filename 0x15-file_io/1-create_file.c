6e56e80afdb679ebaabdd86540cadb263c69ef03
saif-sabit <saifsabit@gmail.com>
2023-04-08 04:59:09 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,19 +12,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd,wc=0;
+	int fd, i = 0, wc = 0;
 
+	while (text_content[i] != '\0')
+		i++;
 	if (!filename)
 		return (-1);
-	fd = open(filename,O_RDWR | O_CREAT | O_TRUNC,0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content)
-	{
-		wc = write(fd,text_content,strlen(text_content));
-		if (wc == -1)
-			return (-1);
-		close(fd);
-	}
-	return(wc);
+	wc = write(fd, text_content, i);
+	if (wc == -1)
+		return (-1);
+	close(fd);
+	return (wc);
 }
