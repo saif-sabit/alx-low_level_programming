ffc609d3887cc29c45247c503822d9cda0e36115
saif-sabit <saifsabit@gmail.com>
2023-04-07 17:06:35 -0700

    REad text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,15 +13,19 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	int n, fd;
+	int n, wn, fd;
+
 	buf = malloc(letters * sizeof(char));
+
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	n = read(fd, buf, letters);
-	write(1, buf, n);
+	wn = write(1, buf, n);
+	if (wn < n)
+		return (0);
 	close(fd);
 	return (n);
 }
