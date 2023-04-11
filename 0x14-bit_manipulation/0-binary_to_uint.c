aa05b0ce813bdf915a17f5920a21636037981933
saif-sabit <saifsabit@gmail.com>
2023-04-11 05:12:50 -0700

    Binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include <math.h>
+#include "main.h"
 /**
  * binary_to_uint- changes from binary to decimal
  * @b: pointer to string of bits
@@ -10,6 +10,8 @@ unsigned int binary_to_uint(const char *b)
 	int n;
 	int i = 0;
 
+	if (!b)
+		return (0);
 	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
@@ -17,6 +19,13 @@ unsigned int binary_to_uint(const char *b)
 		i++;
 	}
 	for (n = 0; n < i; n++)
-		ret += (b[i - 1 - n] - '0') *  pow(2, n);
+		ret += (b[i - 1 - n] - '0') *  power(2, n);
 	return (ret);
+}
+int power(int base, int exponent)
+{
+	int i, result=1;
+	for (i = exponent; i > 0; i--)
+		result = result * base;
+	return (result);
 }
