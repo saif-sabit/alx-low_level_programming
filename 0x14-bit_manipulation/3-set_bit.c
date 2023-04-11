827b3251adaed085ce8b67da9fdd96afdbd1832a
saif-sabit <saifsabit@gmail.com>
2023-04-11 16:34:15 -0700

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+/**
+ *set_bit- sets bit in index
+ * @n: pointer to length
+ * @index: index of needed bit
+ * Return: 1 on success -1 and -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int p;
+
+	if (index > 64)
+		return (-1);
+
+	for (p = 1; index > 0; index--)
+		p *= 2;
+	*n += p;
+	return (1);
+}
