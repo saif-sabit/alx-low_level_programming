9f1d9627e1f6cc3b690c81f8c9fe51cda917087b
saif-sabit <saifsabit@gmail.com>
2023-05-08 03:22:12 -0700

    update Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,9 +15,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 	ssize_t n, wn, fd;
 
-
-	if (filename == NULL)
-		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
