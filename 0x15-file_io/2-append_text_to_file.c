0cbef09c883e1f78b1d4311335c0df0b302c7ef4
saif-sabit <saifsabit@gmail.com>
2023-05-08 00:49:00 -0700

    update Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,14 +11,17 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	while (text_content[i] != '\0')
 		i++;
-	if (!filename)
-		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
 		return (-1);
-	wc = write(fd, text_content, i);
-	if (wc == -1)
+	if (!filename)
 		return (-1);
+	if (text_content)
+	{
+		wc = write(fd, text_content, i);
+		if (wc == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
