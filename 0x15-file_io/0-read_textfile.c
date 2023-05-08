a5b8c73024eaff5ef70c6bc303b6f42b7ce00ef0
saif-sabit <saifsabit@gmail.com>
2023-05-08 15:17:03 -0700

    update Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,8 +14,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	buf = malloc(letters * sizeof(char));
+	if (buf == -1)
+		return (0);
 	n = read(fd, buf, letters);
 	wn = write(1, buf, n);
+	if (n == -1 || wn == -1)
+		return (0);
 	free(buf);
 	close(fd);
 	return (wn);
