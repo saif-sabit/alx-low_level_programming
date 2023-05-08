898f7336010106eb36820e81f4c7449cd283c5de
saif-sabit <saifsabit@gmail.com>
2023-05-08 15:34:08 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	while (text_content[i] != '\0')
-		i++;
+	if (text_content)
+		while (text_content[i] != '\0')
+			i++;
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
