9ae92377e48a3d6d414261d6ff2c538325f8d0ff
saif-sabit <saifsabit@gmail.com>
2023-06-15 07:04:02 -0700

    List file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,4 +1,7 @@
+#ifndef LIST
+#define LIST 
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -23,3 +26,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+#endif
